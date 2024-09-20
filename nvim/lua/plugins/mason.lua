5ca754f0848f1a988d42ee0581046f57a56d8df7
Akihisa Yagi <caphosra@gmail.com>
2024-09-20 15:54:40 +0900

    Add mason-lspconfig

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,9 +1,23 @@
 return {
     {
+        "neovim/nvim-lspconfig"
+    },
+    {
         "williamboman/mason.nvim",
         config = function()
             require("mason").setup()
         end
     },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup()
+            require("mason-lspconfig").setup_handlers {
+                function(serv)
+                    require("lspconfig")[serv].setup {}
+                end
+            }
+        end
+    }
 }
 
